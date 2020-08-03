ee1f5f9e0852738f98830e27935760a2a76f70a4
lakshman <slkumar1703@gmail.com>
2020-08-03 22:50:23 +0530

    udpated python file

--- findvowels.py
+++ findvowels.py
@@ -1,14 +1,2 @@
-vowels = ['a','e','i','o','u']
-s=input("Enter string: ")
-i=0
-v=0
-while i<len(s):
-    c=s[i]
-    i+=1
-    try:
-        v = int(vowels.index(c)>=0)+v
-    except ValueError:
-        v=v
-print(f'The string contains {v} vowels')
-
-
+print ("Hello world")
+print ("This is Lakshmana Kumar")
